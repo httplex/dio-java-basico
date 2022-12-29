e6cc70c3f78031523407cff21f28abb89f31e929
httplex <desufg826@gmail.com>
2022-12-29 10:59:37 -0300

    mais algumas variaveis adicionadas e testadas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+
+        String meuNome = "Alex Silva";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; /* casting */
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
     }
 }
